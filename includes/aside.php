865f5651c9624f591c3086a3a08fdb239dbafbf7
chrillep <christian.widlund@gmail.com>
2012-08-01 14:17:11 +0200

    tog bort bilder blev bara drygt

--- includes/aside.php
+++ includes/aside.php
@@ -4,16 +4,16 @@
 			<div id="textint">
 				<label>
 					<div class="date">
-						  <span class="day"><? echo date("d")?></span> 
-						  <span class="month"><? echo date("M")?></span> 
-						  <span class="year"><? echo date("Y")?></span>
+						  <span class="day"><?php echo date("d")?></span> 
+						  <span class="month"><?php echo date("M")?></span> 
+						  <span class="year"><?php echo date("Y")?></span>
 					</div>
 					<h2>LOGIN</h2>
 				</label>
 			</div>
 			<div id="loginform">
 
-				<form method="POST" action="<?=$_SERVER['PHP_SELF'];?>">
+				<form method="POST" action="<?php=$_SERVER['PHP_SELF'];?>">
 					E-Mail:   <input class="field" type="email" name="username"/>
 					Password: <input class="field" type="password" name="password"/>
 							  <input type="hidden" name="token" value="<?php echo $token?>" />
